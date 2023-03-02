1d5c12fc4b2341b61ecefa52945a1a357a59341d
Jonathan <jonathandpereyra@gmail.com>
2023-03-02 12:33:13 -0300

    commiteado

--- interface.go
+++ interface.go
@@ -2,12 +2,12 @@ package figuras
 
 import "fmt"
 
-type Geometria interface {
+type geometria interface {
 	area() float64
 	perimetro() float64
 }
 
-func Medidas(g Geometria) {
+func Medidas(g geometria) {
 	//fmt.Println(g)
 	fmt.Println("Area: ", g.area())
 	fmt.Println("Perímetro: ", g.perimetro())
